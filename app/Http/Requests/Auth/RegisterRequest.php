49523829204a98c2bb5b81fc6f698c46d9dfcb2f
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2025-07-28 10:48:58 +0800

    feat: register request

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|max:100|min:1',
+            'middle_name' => 'nullable|max:100|min:1',
+            'last_name' => 'required|max:100|min:1',
+            'email' => 'required|max:255|min:1|uniqe:users',
+        ];
+    }
+}
