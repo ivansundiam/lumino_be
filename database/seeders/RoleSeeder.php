8b820127998d2d68e72278f41d0839f20f5083b6
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2025-08-04 16:42:08 +0800

    feat: Role and user role

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            ['name' => 'regular_user', 'description' => 'Regular User'],
+            ['name' => 'moderator', 'description' => 'Moderator'],
+        ];
+
+        foreach($roles as $role) {
+            Role::create($role);
+        }
+    }
+}
